61bf839af6ce0dbccd0af13602f820f183c61ee3
sagoishandsome <167723994+sagoishandsome@users.noreply.github.com>
2026-01-06 21:31:29 +0800

    删除菜品

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -3,7 +3,10 @@ package com.sky.service;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public interface DishService {
 
@@ -16,4 +19,11 @@ public interface DishService {
     public void saveWithFlavor(DishDTO dishDTO);
 
     PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     *根据主键查询
+     * @param ids
+     */
+    @Select("select * from dish where id=#{id}")
+    void deleteBatch(List<Long> ids);
 }
