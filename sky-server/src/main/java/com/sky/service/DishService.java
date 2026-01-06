c309bcc4a6af82f9b5c2415f9827c1bc30c6232f
sagoishandsome <167723994+sagoishandsome@users.noreply.github.com>
2026-01-06 20:16:53 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public interface DishService {
@@ -12,4 +14,6 @@ public interface DishService {
 
 
     public void saveWithFlavor(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
